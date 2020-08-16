653c16c2e38f62ec033487caac5b050f8999425c
Eden_Sharoni1995 <eden.sharoni@s.afeka.ac.il>
2020-08-16 19:48:48 +0300

    Update face_afeka_connect.php

--- face_afeka_connect.php
+++ face_afeka_connect.php
@@ -26,8 +26,6 @@ $conn = new mysqli($servername, $username, $password, $dbname);
 // Check connection
 if ($conn->connect_error)
     die("Connection failed: " . $conn->connect_error);
-else 
-	echo "Connected";
     
 function CalculatePassword($pass)
 {
