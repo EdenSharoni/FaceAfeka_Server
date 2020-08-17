f2499017e676773f31b5edac2c4bc615c33cfe76
Eden_Sharoni1995 <eden.sharoni@s.afeka.ac.il>
2020-08-18 00:10:21 +0300

    Update face_afeka_connect.php

--- face_afeka_connect.php
+++ face_afeka_connect.php
@@ -11,36 +11,34 @@ $username = "root";
 $password = "eden123";
 $dbname = "face_afeka";
 
-$connect = mysqli_connect($servername, $username, $password,$dbname) or die("Unable to connect to '$dbname'");
+$connect = mysqli_connect($servername, $username, $password, $dbname) or die("Unable to connect to '$dbname'");
 // Check connection
-if (mysqli_connect_errno($connect))
-{
+if (mysqli_connect_errno($connect)) {
     echo "Failed to connect to MySQL: " . mysqli_connect_error();
-} 
- /* // Create connection
+} /*
+ * // Create connection
  * $conn = new mysqli($servername, $username, $password, $dbname);
  * // Check connection
  * if ($conn->connect_error)
  * die("Connection failed: " . $conn->connect_error);
- * else {
- * $sql = "SELECT * FROM " . $user_table . " WHERE username='Eden1480'";
- * if (@$conn->query($sql) === TRUE)
- * echo "TRUE " . $sql;
- * else
- * echo "FALSE " . $sql;
- * }
- *
- *
- *
- * if (! isset($user_table) && ! isset($post_table) && ! isset($like_table) && ! isset($friend_table) && ! isset($comment_table) && ! isset($game_table)) {
- * $user_table = "Users";
- * $post_table = "Posts";
- * $like_table = "Likes";
- * $friend_table = "Friends";
- * $comment_table = "Comments";
- * $game_table = "Games";
- * }
  */
+else {
+    $sql = "SELECT * FROM " . $user_table . " WHERE username='Eden1480'";
+    if (@$conn->query($sql) === TRUE)
+        echo "TRUE " . $sql;
+    else
+        echo "FALSE " . $sql;
+}
+
+if (! isset($user_table) && ! isset($post_table) && ! isset($like_table) && ! isset($friend_table) && ! isset($comment_table) && ! isset($game_table)) {
+    $user_table = "Users";
+    $post_table = "Posts";
+    $like_table = "Likes";
+    $friend_table = "Friends";
+    $comment_table = "Comments";
+    $game_table = "Games";
+}
+
 function CalculatePassword($pass)
 {
     $pass = $pass[0] . $pass . $pass[0];
